6eb139e029b051a5e097d232d306892d1cc06b80
Josh Faison <Joshfaiso@yahoo.com>
2020-01-16 23:32:52 -0500

    added in js and npm, response not complete, but almost there

--- Assets/generatehtml.js
+++ Assets/generatehtml.js
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+function generateHtml(data) {
+  return
+  ;`<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -43,13 +45,29 @@
         <div class="card ml-3 text-center user-card">
           <img
             class="shadow mb-5 mx-auto d-block"
-            src="https://avatars3.githubusercontent.com/u/48721774?s=400&u=ce91f87cb9b663b7704759498717c22e03712ffe&v=4"
-            alt=""
+            src="${data.profile.data.avatar_url}"
+            alt="image"
           />
           <div class="card-title">Hi</div>
           <div class="card-body">
-            <h2>My name is</h2>
-            <h4>Currently at @trilogy</h4>
+            <h2>My name is ${data.answers.name}</h2>
+            <h4>Currently at ${data.profile.data.company}</h4>
+            <div class="links-nav">
+            <div class="nav-link">
+            <i class="fas fa-location-arrow"></i>
+            <a href="https://www.google.com/maps/place/${data.profile.data.location}">${data.profile.data.location}</a>
+            </div>
+            <div class="nav-link">
+              <i class="fab fa-github-alt"></i>
+              <a href="${data.profile.data.url}">GitHub</a>
+            </div>
+            <div class="nav-link">
+              <i class="fas fa-blog"></i>
+              <a href="${data.profile.data.blog}">Blog</a>
+            </div>
+          </div>
+        </div>
+      </div>
           </div>
         </div>
       </div>
@@ -57,20 +75,19 @@
         <br />
         <br />
         <br />
-        <h1 class="text-center">
-          <strong>I build things and learn to code</strong>
+        <h1 class="text-center">${data.profile.data.bio}
         </h1>
         <div class="row">
           <div class="col-sm-6">
             <div class="card ml-5 mb-2 text-center">
               <div class="card-title">Public Repositories</div>
-              <div class="card-body">135</div>
+              <div class="card-body"><h3>${data.profile.data.public_repos}</h3></div>
             </div>
           </div>
           <div class="col-sm-6">
             <div class="card ml-2 ml-2 mt-1 text-center">
               <div class="card-title">Followers</div>
-              <div class="card-body">92</div>
+              <div class="card-body"><h3>${data.profile.data.followers}</h3></div>
             </div>
           </div>
         </div>
@@ -79,14 +96,14 @@
             <div class="card ml-5 mt-4 text-center">
               <div class="card-title">
                 Github Stars
-                <div class="card-body">65</div>
+                <div class="card-body"><h3>${data.stars.data.length}</h3></div>
               </div>
             </div>
           </div>
           <div class="col-sm-6">
             <div class="card ml-2 mr-3 mt-4 text-center">
               <div class="card-title">Following</div>
-              <div class="card-body">65</div>
+              <div class="card-body"><h3>${data.profile.data.following}</h3></div>
             </div>
           </div>
         </div>
@@ -99,4 +116,5 @@
       <div class="jumbotron"></div>
     </div>
   </body>
-</html>
+</html>`
+}
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+//globals
+
 const fs = require("fs")
 const inquirer = require("inquirer")
 const util = require("util")
-
+const axios = require("axios")
+const generatehtml = require("./Assets/generatehtml.js")
 const writeFilesAsync = util.promisify(fs.writeFile)
+const readFileAsync = util.promisify(fs.readFile)
+const htmlPdf = require("html-pdf")
+const userinput = {
+  format: "Letter",
+  orientation: "portrait",
+  type: "pdf"
+}
+//inquirer functinon
 
 function promptUser() {
   return inquirer.prompt([
@@ -24,6 +35,8 @@ function promptUser() {
   ])
 }
 
+//pushing the users input into a variable for pdf/errors
+
 promptUser()
   .then(function(answers) {
     const pdf = generatePdf(answers)
@@ -33,3 +46,49 @@ promptUser()
   .catch(function(err) {
     console.log(err)
   })
+
+//function for command line
+
+async function initialize() {
+  try {
+    //storing answers from inquirer
+    const answers = await promptUser()
+    //using Inquirer answers to make api calls
+    const queryURL = `https://api.github.com/users/&${answers.github}`
+    const queryURLStar = `https://api.github.com/uses/&${answers.github}/starred`
+
+    //Making API calls with axios from above info
+    const profile = await axios.get(queryURL)
+    //console.log(profile)
+    const stars = await axios.get(queryURLStar)
+
+    //collecting data into an object
+    const data = {
+      color: answers.color,
+      answers: answers,
+      profile: profile,
+      stars: stars
+    }
+    //console.log(data)
+
+    //calling in seperate js file for the html-pdf
+    const html = generatehtml(data)
+
+    await writeFilesAsync(`./assets/${answers.username}.html`, html)
+    console.log("success!")
+
+    //html-pdf time!!!!
+    htmlPdf
+      .create(html, userinput)
+      .toFile(`.assets/pdf/${answers.username}.pdf`, function(err, res) {
+        if (err) {
+          console.log(err)
+        }
+        console.log(`success!`)
+      })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+initialize()
